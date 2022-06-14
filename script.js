14d8b51fd3fd1d6cc4c279c3153314682e44d2e6
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-14 16:19:25 +0100

    added function to generate random number and mine positions

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
+function randomNum(boardSize) {
 
-function createBoard(boardSize) {
+    return Math.floor(Math.random() * boardSize);
+}
+
+function checkMinePositionExists(positions, newPosition) {
+
+    if(positions.length > 0) {
+        for(let i = 0; i < positions.length; i++) {
+            if(positions[i].x === newPosition.x && positions[i].y === newPosition.y) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function getMinePositions(boardSize) {
+    
+    const positions = [];
+
+    while(positions.length < boardSize) {
+
+        const position = {
+            x: randomNum(boardSize),
+            y: randomNum(boardSize)
+        }
+
+        const minePositionExists = checkMinePositionExists(positions, position);
+        if(!minePositionExists) {
+            positions.push(position);
+        }
+    }
+
+    return positions;
+}
+
+function createBoard(boardSize, numOfMines) {
 
     const board = [];
+    const minePositions = getMinePositions(boardSize);
+    console.log(minePositions);
 
     for(let x = 0; x < boardSize; x++) {
         const row =[];
@@ -28,9 +67,12 @@ function createBoard(boardSize) {
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    const BOARD_SIZE = 10;
+    const NUM_OF_MINES = 10;
+    
     const canvas = document.getElementById("canvas");
 
-    const board = createBoard(10);
+    const board = createBoard(BOARD_SIZE, NUM_OF_MINES);
     
     board.forEach(row => {
         row.forEach(tile => {
