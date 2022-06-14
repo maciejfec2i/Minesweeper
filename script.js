0573eb23e9d7f76b249c48742e77e894ce0ca95a
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-14 12:10:42 +0100

    working on generating a board of elements

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const canavsWidth = 600;
-const canvasHeight = 600;
+
+function createBoard(boardSize) {
+
+    const board = [];
+
+    for(let x = 0; x < boardSize; x++) {
+        const row =[];
+
+        for(let y = 0; y < boardSize; y++) {
+
+            const element = document.createElement("div");
+            element.classList.add("hidden");
+
+            const tile = {
+                element,
+                x,
+                y
+            }
+
+            row.push(tile);
+        }
+
+        board.push(row);
+    }
+
+    return board;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const canvas = document.getElementById("canvas");
-    canvas.width = canavsWidth;
-    canvas.height = canvasHeight;
 })
