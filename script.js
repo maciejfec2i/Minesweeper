8f608517d1ec700aba7f1ddc1a4d2c01d2ec048b
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-14 10:54:38 +0100

    added sizing for canvas element

---
+++ script.js
@@ -0,0 +1,9 @@
+const canavsWidth = 600;
+const canvasHeight = 600;
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const canvas = document.getElementById("canvas");
+    canvas.width = canavsWidth;
+    canvas.height = canvasHeight;
+})
